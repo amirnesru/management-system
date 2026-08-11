7684132d1e4b16f07947a0aa6219ab6f1175e63d
Amir Nesru <amirnesru054@gmail.com>
2026-08-11 22:18:15 +0300

    feat: configure application routing

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,31 +1,86 @@
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import Attendance from "../pages/Attendance";
-import AllMembers from "../pages/AllMembers";
-import Login from "../pages/Login";
-import Signup from "../pages/Signup";
-import Dashboard from "../pages/Dashboard";
-import Settings from "../pages/Settings";
-import NotFound from "../pages/NotFound";
-
+// Layout & Route Guards
+import DashboardLayout from "../layouts/DashboardLayout";
 import ProtectedRoute from "./ProtectedRoute";
 
-function AppRoutes() {
+// Auth Pages
+import Login from "../pages/Login/Login";
+import Signup from "../pages/SignUp/Signup";
+
+// Feature Pages
+import Dashboard from "../pages/Dashboard/Dashboard";
+import AllMembers from "../pages/AllMember/AllMembers";
+import Attendance from "../pages/Attendance/Attendance";
+import Settings from "../pages/Settings/Settings";
+import NotFound from "../pages/NotFound/NotFound";
+import AccessDenied from "../pages/AccessDenied/AccessDenied";
+
+export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      {/* Public Routes */}
+      <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
 
+      {/* All protected routes */}
       <Route element={<ProtectedRoute />}>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/members" element={<AllMembers />} />
-        <Route path="/attendance" element={<Attendance />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route element={<DashboardLayout />}>
+          
+          {/* Dashboard */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute permission="canViewDashboard">
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Members */}
+          <Route
+            path="/members"
+            element={
+              <ProtectedRoute permission="canViewMembers">
+                <AllMembers />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Attendance */}
+          <Route
+            path="/attendance"
+            element={
+              <ProtectedRoute permission="canViewAttendance">
+                <Attendance />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Settings */}
+          <Route
+            path="/settings"
+            element={
+              <ProtectedRoute permission="canAccessSettings">
+                <Settings />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+
+        {/* Access Denied */}
+        <Route path="/access-denied" element={<AccessDenied />} />
       </Route>
 
+      {/* Default */}
+      <Route
+        path="/"
+        element={<Navigate to="/dashboard" replace />}
+      />
+
+      {/* 404 */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
-
-export default AppRoutes;
